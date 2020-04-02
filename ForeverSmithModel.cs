69abd241e0b9897960ee6e1a85a48e79f3e4aafc
calsev <calsev@gmail.com>
2020-04-01 21:19:27 -0800

    Add mod

---
+++ ForeverSmithModel.cs
@@ -0,0 +1,19 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+using TaleWorlds.Core;
+
+namespace SmithForever
+{
+	public class ForeverSmithModel : DefaultSmithingModel
+	{
+		public override int GetEnergyCostForRefining(ref Crafting.RefiningFormula refineFormula, Hero hero)
+		{
+			return 0;
+		}
+
+		public override int GetEnergyCostForSmithing(ItemObject item, Hero hero)
+		{
+			return 0;
+		}
+	}
+}
---
+++ ForeverSmithSubModule.cs
@@ -0,0 +1,16 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace SmithForever
+{
+	public class ForeverSmithSubModule : MBSubModuleBase
+	{
+		protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+		{
+			if (!(game.GameType is Campaign))
+				return;
+			gameStarterObject.AddModel(new ForeverSmithModel());
+		}
+	}
+}
