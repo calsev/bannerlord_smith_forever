2cd21666f404f0c5fa692c2935b58971681054a4
calsev <calsev@gmail.com>
2020-04-06 19:32:47 -0800

    Initialize logging on module load

--- ForeverSmithSubModule.cs
+++ ForeverSmithSubModule.cs
@@ -7,6 +7,28 @@ namespace SmithForever
 {
 	public class ForeverSmithSubModule : MBSubModuleBase
 	{
+		protected static readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
+		protected override void OnSubModuleLoad()
+		{
+			NLog.Config.LoggingConfiguration logConfig = new NLog.Config.LoggingConfiguration();
+			NLog.Targets.FileTarget logFile = new NLog.Targets.FileTarget(LogFileTarget()) { FileName = LogFilePath() };
+
+			logConfig.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logFile);
+			NLog.LogManager.Configuration = logConfig;
+		}
+
+		protected virtual string LogFileTarget()
+		{
+			return "ForeverSmithLogFile";
+		}
+
+		protected virtual string LogFilePath()
+		{
+			// The default, relative path will place the log in $(GameFolder)\bin\Win64_Shipping_Client\
+			return "ForeverSmithLog.txt";
+		}
+
 		protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
 		{
 			if (!(game.GameType is Campaign))
